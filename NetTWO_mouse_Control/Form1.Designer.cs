240a654f9f0f9695e2d8cd17b044f1f1d2b9d028
robinlyang <robiny@gmail.com>
2016-12-05 20:05:37 -0700

    more

--- NetTWO_mouse_Control/Form1.Designer.cs
+++ NetTWO_mouse_Control/Form1.Designer.cs
@@ -29,47 +29,41 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.output = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(533, 12);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(152, 145);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(348, 233);
+            this.pictureBox1.Size = new System.Drawing.Size(621, 403);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(30, 90);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(582, 45);
+            this.output.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(645, 469);
+            this.Controls.Add(this.output);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -78,10 +72,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox output;
     }
 }
 
--- NetTWO_mouse_Control/Form1.cs
+++ NetTWO_mouse_Control/Form1.cs
@@ -12,6 +12,9 @@ namespace NetTWO_mouse_Control
 {
     public partial class Form1 : Form
     {
+        public string number1 = "", number2 = "", oper = "";
+        public double total, value1, value2;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,14 +22,62 @@ namespace NetTWO_mouse_Control
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            textBox1.Text = e.X.ToString();
-            textBox2.Text = e.Y.ToString();
+            //textBox1.Text = e.X.ToString();
+            //textBox2.Text = e.Y.ToString();
         }
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            textBox1.Text = e.X.ToString();
-            textBox2.Text = e.Y.ToString();
+            //textBox1.Text = e.X.ToString();
+            //textBox2.Text = e.Y.ToString();
+
+            //all the calculator code is within the picturbox
+            //check for operator
+            //+ = (381, 329), (473, 383)
+            if (e.X >=381 && e.X <= 473 && e.Y >=329 && e.Y <=383)
+            {
+                oper = "+";
+            }
+            //check for equal
+            //= = (200, 329), (597, 386)
+            if (e.X >= 200 && e.X <= 597 && e.Y >= 329 && e.Y <= 386)
+            {
+                value1 = Convert.ToDouble(number1);
+                value2 = Convert.ToDouble(number2);
+                if (oper=="+")
+                {
+                    total = value1 + value2;
+                }
+
+                output.Text = total.ToString();
+                number1 = "";
+                number2 = "";
+                oper = "";
+            }
+            //check for 1 pressed
+            //1 = (20, 259), (115, 316)
+            if (e.X >= 20 && e.X <= 259 && e.Y >= 115 && e.Y <= 316)
+            {
+                if(oper=="")
+                {
+                    number1 = number1 + "1";
+                    output.Text = number1;
+                }
+                else
+                {
+                    number2 = number2 + "1";
+                    output.Text = number2;
+                }
+                
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
